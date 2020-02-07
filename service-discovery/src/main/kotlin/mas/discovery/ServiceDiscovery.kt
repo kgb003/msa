29e20e611d159ec99eb7b9807e4fb45a8afd6fea
mz-tiger-at-533088845121 <DDUL1r3X7M5Oe5knvro9v1/OlQet5FSfVadCMDl2OZo=>
2020-02-07 14:03:47 +0900

    2일차 커밋

---
+++ service-discovery/src/main/kotlin/mas/discovery/ServiceDiscovery.kt
@@ -0,0 +1,13 @@
+package mas.discovery
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer
+
+@EnableEurekaServer
+@SpringBootApplication
+class ServiceDiscovery
+
+fun main(args: Array<String>) {
+    runApplication<ServiceDiscovery>(*args)
+}
