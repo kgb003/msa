8284253a11449a86d8fe8677feb9d35209ba1939
mz-tiger-at-533088845121 <DDUL1r3X7M5Oe5knvro9v1/OlQet5FSfVadCMDl2OZo=>
2020-02-14 10:38:49 +0900

    config 변경 테스트

--- accounts-service/src/main/kotlin/msa/accounts/AccountsService.kt
+++ accounts-service/src/main/kotlin/msa/accounts/AccountsService.kt
@@ -1,14 +1,28 @@
 package msa.accounts
 
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
 
 @EnableEurekaClient
 @SpringBootApplication
-class AccountsService {
-}
+class AccountsService
 
 fun main(args: Array<String>) {
     runApplication<AccountsService>(*args)
+}
+
+@RestController
+class TestController(
+    @Value("\${say.cheese}")
+    private val say: String
+) {
+
+    @GetMapping("/say")
+    fun say(): String {
+        return say
+    }
 }
