8b6872dff1d0d39a31721fb723dc0cd0004586ac
mz-tiger-at-533088845121 <DDUL1r3X7M5Oe5knvro9v1/OlQet5FSfVadCMDl2OZo=>
2020-02-18 18:19:08 +0900

    gateway service 커밋

---
+++ gateway-service/src/main/kotlin/msa/GatewayService.kt
@@ -0,0 +1,54 @@
+package msa
+
+import com.netflix.zuul.ZuulFilter
+import com.netflix.zuul.context.RequestContext
+import org.slf4j.LoggerFactory
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableZuulProxy
+class GatewayService
+
+fun main(args: Array<String>) {
+    runApplication<GatewayService>(*args)
+}
+
+@Configuration
+class GatewayConfig {
+
+    @Bean
+    fun preFilter(): ZuulFilter = PreFilter()
+}
+
+class PreFilter: ZuulFilter() {
+
+    private val logger = LoggerFactory.getLogger(this.javaClass)
+
+    override fun run(): Any? {
+        val ctx = RequestContext.getCurrentContext()
+        val request = ctx.request
+        logger.info("~~~~~~~~~~~~~~~~~~~~~$request")
+
+        return null
+    }
+
+    override fun shouldFilter(): Boolean {
+        return true
+    }
+
+    override fun filterType(): String {
+        // pre, routing, post, error
+        return "pre"
+    }
+
+    override fun filterOrder(): Int {
+        return 0
+    }
+
+}
